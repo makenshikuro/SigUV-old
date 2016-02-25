2cc68c5690043c0f57e5e176529baa75d0963c07
Estudio <Estudio@Estudio-PC>
2016-02-25 23:44:00 +0100

    Añadidos Sidebar para Capas y para Facultades
    Añadidos inputs para Busqueda
    
    Añadidos botones para Capas (funcionan)

--- web/js/mapas.js
+++ web/js/mapas.js
@@ -10,18 +10,23 @@
 function init() {
 
     /* Variables  */
-    var mapBounds = new L.LatLngBounds(
-            new L.LatLng(39.511948, -0.425012),
-            new L.LatLng(39.513769, -0.422732));
+    var surOeste = new L.LatLng(39.511948, -0.425012);
+    var norEste = new L.LatLng(39.513769, -0.422732);
+    var mapBounds = new L.LatLngBounds(surOeste, norEste);
     var mapMinZoom = 5;
     var mapMaxZoom = 25;
-
-
-    map = L.map('map', {center: [39.512859, -0.4244782], zoom: 18});
-
+    var centro = [39.512859, -0.4244782];
     L.mapbox.accessToken = 'pk.eyJ1IjoidWJ1c3R1cyIsImEiOiJjaWs2bjhidDMwMHc1cDdrc2o4cnpkdWhkIn0.Nrk8FVCyADAGWSIGm86yBQ';
-    // Replace 'mapbox.streets' with your map id.
 
+    /* Inicialización Mapa */
+    map = L.map('map', {center: centro, zoom: 18, zoomControl: false});
+
+    /* Capas 
+     * mapboxTiles = Mapbox.com  
+     * osm = Open Street View    
+     * googleLayer = Google Maps 
+     * PB = Planos planta Baja   
+     */
     mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/ubustus.p2c7e9pf/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
         attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>',
         minZoom: mapMinZoom,
@@ -32,23 +37,23 @@ function init() {
 
     googleLayer = new L.Google('ROADMAP');
 
-
-
-
     osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: mapMinZoom,
         maxZoom: mapMaxZoom,
         unloadInvisibleTiles: true,
         opacity: 1.00
     });
-    custom = L.tileLayer('http://www.adretse.es/mapEtse/{z}/{x}/{y}.png', {
+    PB = L.tileLayer('http://www.adretse.es/maps/{z}/{x}/{y}.png', {
         minZoom: mapMinZoom,
         maxZoom: mapMaxZoom,
         bounds: mapBounds,
         zIndex: 7
     });
 
-    // control
+    /* Añadido de Capas */
+    map.addLayer(mapboxTiles);
+    map.addLayer(PB);
+    //map.addControl(new L.Control.Layers({'OSM': osm, 'MBT': mapboxTiles, 'Google Terrain': googleLayer}, {'ETSE': PB, "Markers": layerGroup}));
 
 
     //Sidebar
@@ -57,17 +62,26 @@ function init() {
         position: 'left'
     });
     map.addControl(sidebarLayers);
-    
+
     sidebarFacultades = L.control.sidebar('sidebarFacultades', {
         closeButton: true,
         position: 'left'
     });
     map.addControl(sidebarFacultades);
 
-
+    /*
+     * Grupos de capas de Marcadores JSON
+     */
     layerGroup = L.layerGroup().addTo(map);
+    layerGroupC = L.layerGroup().addTo(map);
 
-    //GeoJSON
+
+
+    /* Marcadores GeoJSON
+     *  Facultades:   Marcadores correspondientes a las facultades de la UV
+     *  Campus:       Marcadores correspondientes a los campus de la UV
+     * 
+     */
     L.geoJson(Facultades, {
         pointToLayer: function (feature, coordinates) {
             var geoMarker = (L.marker(coordinates, {icon: L.AwesomeMarkers.icon({icon: 'graduation-cap', markerColor: 'red', prefix: 'fa', iconColor: 'black'})}));
@@ -76,6 +90,22 @@ function init() {
         },
         onEachFeature: onEachFeature
     }).addTo(map);
+
+    L.geoJson(Campus, {
+        pointToLayer: function (feature, coordinates) {
+            var geoMarker = (L.marker(coordinates, {icon: L.AwesomeMarkers.icon({icon: 'university', markerColor: 'green', prefix: 'fa', iconColor: 'black'})}));
+            layerGroupC.addLayer(geoMarker);
+            return geoMarker;
+        },
+        onEachFeature: onEachFeature
+    }).addTo(map);
+    
+    /*
+     * Funcion onEachFeature
+     * @param {type} feature: características del JSON
+     * @param {type} layer:   capas
+     * 
+     */
 
     function onEachFeature(feature, layer) {
         var popupContent = "<p>" + feature.properties.name + "</p>";
@@ -91,93 +121,134 @@ function init() {
     /*map.on('click', function(e) {
      alert(e.latlng);
      });*/
-    map.addLayer(mapboxTiles);
-    map.addLayer(custom);
-    map.addControl(new L.Control.Layers({'OSM': osm, 'MBT': mapboxTiles, 'Google Terrain': googleLayer}, {'ETSE': custom, "Markers": layerGroup}));
-    
-}
-function hdd() {
-    
-    sidebarLayers.show();
-}
 
-function hdd2() {
-    
-    sidebarFacultades.show();
-}
-
-
-$(document).ready(function(){
-    $("input[name=icons]").change(function(){
-       
-        var iconos =  $('input:radio[name=icons]:checked').attr("value");
-        if (iconos === "on"){
-            map.addLayer(layerGroup);
+    /*
+     * Función tras cambiar zoom en el mapa
+     * Se ocultan y muestran las capas ...... y ....
+     */
+    map.on('zoomend', function () {
+        // here's where you decided what zoom levels the layer should and should
+        // not be available for: use javascript comparisons like < and > if
+        // you want something other than just one zoom level, like
+        // (map.getZoom > 10)
+        console.log(map.getZoom());
+        if (map.getZoom() === 13) {
+            // setFilter is available on L.mapbox.featureLayers only. Here
+            // we're hiding and showing the default marker layer that's attached
+            // to the map - change the reference if you want to hide or show a
+            // different featureLayer.
+            // If you want to hide or show a different kind of layer, you can use
+            // similar methods like .setOpacity(0) and .setOpacity(1)
+            // to hide or show it.
+            //map.featureLayer.setFilter(function() { return true; });
+        } else {
+            //map.featureLayer.setFilter(function() { return false; });
         }
-        else{
-            
+    });
+
+}
+
+/* Funciones */
+
+
+ /* Funcion openSidebarLayers
+  * Abre un módulo lateral con opciónes 
+  */
+function openSidebarLayers() {
+    sidebarFacultades.hide();
+    sidebarLayers.toggle();
+    }
+
+
+ /* Funcion openSidebarFacultades
+  * Abre un módulo lateral con listado de Facultades 
+  */
+function openSidebarFacultades() {
+    sidebarLayers.hide();
+    sidebarFacultades.toggle();
+}
+
+ /* Funcion closeAllSidebars
+  * Cierra todos los sidebar existentes 
+  */
+function closeAllSidebars() {
+    sidebarFacultades.hide();
+    sidebarLayers.hide();
+}
+
+ /* Funciones JQuery
+  * Disparadores para los cambios de estado de los
+  * interruptores de las capas
+  */
+$(document).ready(function () {
+
+    $("input[name=icons]").change(function () {
+
+        var iconos = $('input:radio[name=icons]:checked').attr("value");
+        if (iconos === "on") {
+            map.addLayer(layerGroup);
+        } else {
+
             map.removeLayer(layerGroup);
         }
         //alert("The text has been changed."+iconos);
-        
-        
-        
+
+
+
     });
-    $("input[name=tipologia]").change(function(){
-       
-        var iconos =  $('input:radio[name=tipologia]:checked').attr("value");
-        if (iconos === "on"){
-            map.addLayer(custom);
-        }
-        else{
-            
-            map.removeLayer(custom);
+    $("input[name=tipologia]").change(function () {
+
+        var iconos = $('input:radio[name=tipologia]:checked').attr("value");
+        if (iconos === "on") {
+            map.addLayer(PB);
+        } else {
+
+            map.removeLayer(PB);
         }
         //alert("The text has been changed."+iconos);
-        
-        
-        
+
+
+
     });
-    $("input[name=fondo]").change(function(){
-       
-        var iconos =  $('input:radio[name=fondo]:checked').attr("value");
-        if (iconos === "on"){
+    $("input[name=fondo]").change(function () {
+
+        var iconos = $('input:radio[name=fondo]:checked').attr("value");
+        if (iconos === "on") {
             map.removeLayer(osm);
             map.addLayer(mapboxTiles);
-        }
-        else{
+        } else {
             map.removeLayer(mapboxTiles);
             map.addLayer(osm);
-            
+
         }
         //alert("The text has been changed."+iconos);
-        
-        
-        
+
+
+
     });
-    
+
 });
 
 /*
-function toogleIcons(){
-    
-    
-    
-    $('#iconsOFF').on('click',function (e){
-    //$("#building").click(function (event) {
-        e.preventDefault();
-        
-        alert('hala');
-        if (aux) {
-            //$(this).removeClass('selected');
-            map.removeLayer(layerGroup);
-            
-
-        } else {
-            map.addLayer(layerGroup);
-            //$(this).addClass('selected');
-      
-        }
-    });
-    aux = !aux;
-}*/
+ function toogleIcons(){
+ 
+ 
+ 
+ $('#iconsOFF').on('click',function (e){
+ //$("#building").click(function (event) {
+ e.preventDefault();
+ 
+ alert('hala');
+ if (aux) {
+ //$(this).removeClass('selected');
+ map.removeLayer(layerGroup);
+ 
+ 
+ } else {
+ map.addLayer(layerGroup);
+ //$(this).addClass('selected');
+ 
+ }
+ });
+ aux = !aux;
+ }*/
--- web/js/poi.js
+++ web/js/poi.js
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 
-var Facultades = [{
+Facultades = [{
     "type": "Feature",
     "properties": {
-        "name": "Escola Tècnica Superior d'Enginyeria",
-        "name_ES": "Escuela Técnica Superior de Ingeniería",
+        "name": "Escola T&egrave;cnica Superior d'Enginyeria",
+        "name_ES": "Escuela T&eacute;cnica Superior de Ingenier&iacute;a",
         "name_EN": "School of Engineering",
         "popupContent": "This is where the Rockies play!"
     },
@@ -19,7 +19,7 @@ var Facultades = [{
 }, {
     "type": "Feature",
     "properties": {
-        "name": "Facultat de Farmàcia",
+        "name": "Facultat de Farm&agrave;cia",
         "name_ES": "Facultad de Farmacia",
         "name_EN": "Faculty of Pharmacy",
         "popupContent": "This is where the Rockies play!"
@@ -31,7 +31,7 @@ var Facultades = [{
 },{
     "type": "Feature",
     "properties": {
-        "name": "Facultat de Matemàtiques",
+        "name": "Facultat de Matem&agrave;tiques",
         "name_ES": "Facultad de Matemáticas",
         "name_EN": "Faculty of Mathematics",
         "popupContent": "This is where the Rockies play!"
@@ -43,7 +43,7 @@ var Facultades = [{
 },{
     "type": "Feature",
     "properties": {
-        "name": "Facultat de Ciencies Biològiques",
+        "name": "Facultat de Ciencies Biol&ograve;giques",
         "name_ES": "Facultad de Ciencias Biológicas",
         "name_EN": "Faculty of Biological Sciencies",
         "popupContent": "This is where the Rockies play!"
@@ -55,7 +55,7 @@ var Facultades = [{
 },{
     "type": "Feature",
     "properties": {
-        "name": "Facultat de Física",
+        "name": "Facultat de F&iacute;sica",
         "name_ES": "Facultad de Física",
         "name_EN": "Faculty of Physics",
         "popupContent": "This is where the Rockies play!"
@@ -67,7 +67,7 @@ var Facultades = [{
 },{
     "type": "Feature",
     "properties": {
-        "name": "Facultat de Química",
+        "name": "Facultat de Qu&iacute;mica",
         "name_ES": "Facultad de Química",
         "name_EN": "Faculty of Chemistry",
         "popupContent": "This is where the Rockies play!"
@@ -75,5 +75,42 @@ var Facultades = [{
     "geometry": {
         "type": "Point",
         "coordinates": [-0.419312, 39.507786]
+    }
+}];
+Campus = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Campus Blasco Iba&ntilde;ez",
+        "name_ES": "Campus Blasco Ibañez",
+        "name_EN": "Campus Blasco Ibañez",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-0.421388, 39.509518 ]
+    }
+},{
+    "type": "Feature",
+    "properties": {
+        "name": "Campus Burjassot-Paterna",
+        "name_ES": "Campus Burjassot-Paterna",
+        "name_EN": "Campus Burjassot-Paterna",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [ -0.360882, 39.477835]
+    }
+},{
+    "type": "Feature",
+    "properties": {
+        "name": "Campus Tarongers",
+        "name_ES": "Campus Tarongers",
+        "name_EN": "Campus Tarongers",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-0.341757, 39.4782067]
     }
 }];
