cbe578ca35da8839bd58781110e8a163fff4fe68
Estudio <Estudio@Estudio-PC>
2016-03-07 00:23:27 +0100

    Añadidos Raphael layer Añadido Google Layer Añadidos nuevos mapas Añadidos botones de niveles por zoom.

--- web/js/mapas.js
+++ web/js/mapas.js
@@ -7,8 +7,16 @@
 
 /* global L, Facultades, restaurants, map */
 
-function init() {
 
+
+function init() {
+    
+    /* Variables Globales   */
+    _fondo = true;
+    _tema = true;
+    _iconos = true;
+    _nivel = "PB";
+    
     /* Variables  */
     var surOeste = new L.LatLng(39.511948, -0.425012);
     var norEste = new L.LatLng(39.513769, -0.422732);
@@ -17,6 +25,7 @@ function init() {
     var mapMaxZoom = 25;
     var centro = [39.512859, -0.4244782];
     L.mapbox.accessToken = 'pk.eyJ1IjoidWJ1c3R1cyIsImEiOiJjaWs2bjhidDMwMHc1cDdrc2o4cnpkdWhkIn0.Nrk8FVCyADAGWSIGm86yBQ';
+
 
     /* Inicialización Mapa */
     map = L.map('map', {center: centro, zoom: 18, zoomControl: false});
@@ -35,7 +44,18 @@ function init() {
         opacity: 1.00
     });
 
-    googleLayer = new L.Google('ROADMAP');
+    googleLayer = new L.Google('SATELLITE');
+    
+    var points = [];
+    //points.push(centro);
+    points.push(new L.LatLng(39.51259, -0.42414));
+    points.push(new L.LatLng(39.51256, -0.42409));
+    points.push(new L.LatLng(39.51251, -0.42416));
+    points.push(new L.LatLng(39.51253, -0.42419));
+    var p = new R.Polygon(points);
+    map.addLayer(p);
+    //map.addLayer(new R.Marker(centro));
+    
 
     osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: mapMinZoom,
@@ -43,7 +63,13 @@ function init() {
         unloadInvisibleTiles: true,
         opacity: 1.00
     });
-    PB = L.tileLayer('http://www.adretse.es/maps/{z}/{x}/{y}.png', {
+    /*PB = L.tileLayer('http://www.adretse.es/mapEtse/{z}/{x}/{y}.png', {
+        minZoom: mapMinZoom,
+        maxZoom: mapMaxZoom,
+        bounds: mapBounds,
+        zIndex: 7
+    });*/
+    PB = L.tileLayer('http://www.adretse.es/siguv/bc0/{z}/{x}/{y}.png', {
         minZoom: mapMinZoom,
         maxZoom: mapMaxZoom,
         bounds: mapBounds,
@@ -53,7 +79,7 @@ function init() {
     /* Añadido de Capas */
     map.addLayer(mapboxTiles);
     map.addLayer(PB);
-    //map.addControl(new L.Control.Layers({'OSM': osm, 'MBT': mapboxTiles, 'Google Terrain': googleLayer}, {'ETSE': PB, "Markers": layerGroup}));
+    
 
 
     //Sidebar
@@ -75,7 +101,7 @@ function init() {
     layerGroup = L.layerGroup().addTo(map);
     layerGroupC = L.layerGroup().addTo(map);
 
-
+    map.addControl(new L.Control.Layers({'OSM': osm, 'MBT': mapboxTiles, 'Google Terrain': googleLayer}, {'ETSE': PB, "Markers": layerGroup}));
 
     /* Marcadores GeoJSON
      *  Facultades:   Marcadores correspondientes a las facultades de la UV
@@ -132,7 +158,10 @@ function init() {
         // you want something other than just one zoom level, like
         // (map.getZoom > 10)
         console.log(map.getZoom());
-        if (map.getZoom() === 13) {
+        if (map.getZoom() < 18) {
+            $("#nivel").removeClass('level');
+            $("#nivel").addClass('levelOFF');
+            
             // setFilter is available on L.mapbox.featureLayers only. Here
             // we're hiding and showing the default marker layer that's attached
             // to the map - change the reference if you want to hide or show a
@@ -141,7 +170,12 @@ function init() {
             // similar methods like .setOpacity(0) and .setOpacity(1)
             // to hide or show it.
             //map.featureLayer.setFilter(function() { return true; });
-        } else {
+        } 
+        if(map.getZoom() >= 18){
+            $("#nivel").addClass('level');
+            $("#nivel").removeClass('levelOFF');
+        }
+        else {
             //map.featureLayer.setFilter(function() { return false; });
         }
     });
@@ -187,9 +221,11 @@ $(document).ready(function () {
         var iconos = $('input:radio[name=icons]:checked').attr("value");
         if (iconos === "on") {
             map.addLayer(layerGroup);
+            _iconos = true;
         } else {
 
             map.removeLayer(layerGroup);
+            _iconos = false;
         }
         //alert("The text has been changed."+iconos);
 
@@ -201,9 +237,11 @@ $(document).ready(function () {
         var iconos = $('input:radio[name=tipologia]:checked').attr("value");
         if (iconos === "on") {
             map.addLayer(PB);
+            _tema = true;
         } else {
 
             map.removeLayer(PB);
+            _tema = false;
         }
         //alert("The text has been changed."+iconos);
 
@@ -216,11 +254,42 @@ $(document).ready(function () {
         if (iconos === "on") {
             map.removeLayer(osm);
             map.addLayer(mapboxTiles);
+            _fondo = true;
         } else {
             map.removeLayer(mapboxTiles);
             map.addLayer(osm);
+            _fondo = false;
 
         }
+        //alert("The text has been changed."+iconos);
+
+
+
+    });
+    
+    
+    $("input[name=nivel]").change(function () {
+
+        var iconos = $('input:radio[name=nivel]:checked').attr("value");
+        if (iconos === "PB") {
+            map.addLayer(layerGroup);
+            _nivel = "PB";
+        }
+        if (iconos === "P1") {
+            map.addLayer(layerGroup);
+            _nivel = "P1";
+        }
+        if (iconos === "P2") {
+            map.addLayer(layerGroup);
+            _nivel = "P2";
+        }
+        if (iconos === "P3") {
+            map.addLayer(layerGroup);
+            _nivel = "P3";
+        }
+        
+        
+        
         //alert("The text has been changed."+iconos);
 
 
